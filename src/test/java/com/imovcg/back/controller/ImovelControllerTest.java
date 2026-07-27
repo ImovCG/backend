8078c8dd21d84ebd91ea15dd0cbbbf870a56b9af
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2026-07-27 14:37:32 -0300

    fix: build dos testes

--- src/test/java/com/imovcg/back/controller/ImovelControllerTest.java
+++ src/test/java/com/imovcg/back/controller/ImovelControllerTest.java
@@ -10,8 +10,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,7 +26,7 @@ class ImovelControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ImovelService imovelService;
 
     @Test
